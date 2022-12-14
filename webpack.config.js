25e4ce655e8544a990a701c65f447fc6313bcf49
serghi.p <piedrabuena.sergio.f@gmail.com>
2022-12-14 17:56:07 -0300

    fixing invalid host header. adding hotmodulerlepacementplugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,69 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js',
-		publicPath: '/'
-	},
-	mode: 'development',
-	resolve: {
-		extensions: ['.js', '.jsx'],
-		alias: {
-			'@components': path.resolve(__dirname, 'src/components/'),
-			'@containers': path.resolve(__dirname, 'src/containers/'),
-			'@pages': path.resolve(__dirname, 'src/pages/'),
-			'@hooks': path.resolve(__dirname, 'src/hooks/'),
-			'@routes': path.resolve(__dirname, 'src/routes/'),
-			'@styles': path.resolve(__dirname, 'src/styles/'),
-			'@icons': path.resolve(__dirname, 'src/assets/icons/'),
-			'@logos': path.resolve(__dirname, 'src/assets/logos/'),
-			'@images': path.resolve(__dirname, 'src/assets/images/')
-		}
-	},
+	entry: "./src/index.js",
 	module: {
 		rules: [
 			{
 				test: /\.(js|jsx)$/,
 				exclude: /node_modules/,
 				use: {
-					loader: 'babel-loader'
-				}
+					loader: "babel-loader",
+				},
 			},
 			{
 				test: /\.html$/,
 				use: [
 					{
-						loader: 'html-loader'
-					}
-				]
-			},
-			{
-				test: /\.(css|scss)$/,
-				use: [
-					"style-loader",
-					"css-loader",
-					"sass-loader",
+						loader: "html-loader",
+					},
 				],
 			},
 			{
+				test: /\.(css|scss)$/,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+			{
 				test: /\.(png|svg|jpg|gif)$/,
-				type: 'asset'
-			}
-		]
+				type: "asset",
+			},
+		],
 	},
+	resolve: {
+		extensions: ["*", ".js", ".jsx"],
+		alias: {
+			"@components": path.resolve(__dirname, "src/components/"),
+			"@containers": path.resolve(__dirname, "src/containers/"),
+			"@pages": path.resolve(__dirname, "src/pages/"),
+			"@hooks": path.resolve(__dirname, "src/hooks/"),
+			"@routes": path.resolve(__dirname, "src/routes/"),
+			"@styles": path.resolve(__dirname, "src/styles/"),
+			"@icons": path.resolve(__dirname, "src/assets/icons/"),
+			"@logos": path.resolve(__dirname, "src/assets/logos/"),
+			"@images": path.resolve(__dirname, "src/assets/images/"),
+		},
+	},
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js",
+		publicPath: "/",
+	},
+	mode: "development",
 	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
 		new HtmlWebpackPlugin({
-			template: './public/index.html',
-			filename: './index.html'
+			template: "./public/index.html",
+			filename: "./index.html",
 		}),
 		new MiniCssExtractPlugin({
-			filename: '[name].css'
+			filename: "[name].css",
 		}),
 	],
-}
+	devServer: {
+		static: "./dist",
+		hot: true,
+	}
+};
